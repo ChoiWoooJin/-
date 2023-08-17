d3d1a54b94ef8c820780c7e2dbf6d3624c9fe1ae
이재명 <iamwoaud1004@naver.com>
2023-08-17 17:22:22 +0900

    [BE] fix - 코드 정리 이재명: 필요없는 코드 정리하였습니다.

--- back-end/src/websocket/websocket.gateway.ts
+++ back-end/src/websocket/websocket.gateway.ts
@@ -1,9 +1,19 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect, MessageBody, ConnectedSocket } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { Server, WebSocket } from 'ws';
 
 @WebSocketGateway(6001)
-export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class WebsocketGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer() server: Server;
 
   private clients: Record<string, WebSocket> = {};
@@ -22,13 +32,18 @@ export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnec
     }
   }
 
-
   @SubscribeMessage('video')
-  handleVideo( @ConnectedSocket() client: WebSocket,@MessageBody() data: any): void {
+  handleVideo(
+    @ConnectedSocket() client: WebSocket,
+    @MessageBody() data: any,
+  ): void {
     const buffer = Buffer.from(data, 'base64');
     for (const sessionId in this.clients) {
-      if ( this.clients[sessionId] !== client && this.clients[sessionId].readyState === WebSocket.OPEN) {
-         this.clients[sessionId].send(buffer);
+      if (
+        this.clients[sessionId] !== client &&
+        this.clients[sessionId].readyState === WebSocket.OPEN
+      ) {
+        this.clients[sessionId].send(buffer);
       }
     }
   }
@@ -39,50 +54,8 @@ export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnec
   }
 
   private findSessionIdByClient(client: WebSocket): string | undefined {
-    return Object.keys(this.clients).find(sessionId => this.clients[sessionId] === client);
+    return Object.keys(this.clients).find(
+      (sessionId) => this.clients[sessionId] === client,
+    );
   }
 }
-
-
-// import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, MessageBody } from '@nestjs/websockets';
-// import { Server, WebSocket } from 'ws';
-
-// @WebSocketGateway(6001)
-// export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
-//   @WebSocketServer() server: Server;
-
-//   handleConnection(client: WebSocket) {
-//     console.log(`Client connected: ${client}`);
-//   }
-
-//   handleDisconnect(client: WebSocket) {
-//     console.log(`Client disconnected: ${client}`);
-//   }
-
-//   @SubscribeMessage('video')
-//   handleVideo(client: WebSocket, @MessageBody() data: ArrayBuffer): void {
-//     // 영상 데이터를 다른 클라이언트에게 브로드캐스트
-//     this.server.clients.forEach((client) => {
-//       if (client.readyState === WebSocket.OPEN) {
-//         client.send(data);
-//       }
-//     });
-//   }
-
-//   @SubscribeMessage('command')
-//   handleMessage(client: WebSocket, @MessageBody() payload: any): void {
-//     try {
-//       const key = payload.key;
-//       console.log(`Received key from client: ${key}`);
-//       // 메시지 처리 로직을 추가합니다.
-//     } catch (error) {
-//       console.error('Error handling message:', error);
-//       // 오류 처리 로직을 추가합니다.
-//     }
-//     this.server.clients.forEach((client) => {
-//       if(client.readyState === WebSocket.OPEN){
-//         client.send(payload);
-//       }
-//     });
-//   }
-// }
